791bc8a07e6c209ed5ecfd7eaefb3305d927ac24
Ricardo Saravia <saraviamade@gmail.com>
2022-10-24 17:50:11 -0500

    avance 1

---
+++ WCF_Mant/IServicioVehiculos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Mant
+{
+    // NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioVehiculos" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioVehiculos
+    {
+        [OperationContract]
+        List<VehiculoDC> ListarVehiculosFechas(String strVehiculo, DateTime fecini, DateTime fecfin);      
+    }
+
+    [DataContract]
+    [Serializable]
+    public class VehiculoDC
+    {
+        [DataMember]
+        public String idVehiculo { get; set; }
+        
+        [DataMember]
+        public DateTime Fec_Mant_Inic { get; set; }
+        [DataMember]
+        public DateTime Fec_Mant_Fin { get; set; }
+        [DataMember]
+        public Single tipo_mantenimiento { get; set; }
+        [DataMember]
+        public String TipoMantenimiento { get; set; }
+        [DataMember]        
+        public String Nom_mec { get; set; }
+        [DataMember]
+        public String Ape_mec { get; set; }
+        [DataMember]
+        public String idMecanico  { get; set; }
+        [DataMember]
+        public Single  Tipo_Trans  { get; set; }
+        [DataMember]
+        public String TipoTransmision { get; set; }
+        [DataMember]
+        public String idMarca { get; set; }
+        [DataMember]
+        public Single Tipo_Unidad{ get; set; }
+        
+        [DataMember]
+        public String  TipoUnidad { get; set; }
+        [DataMember]
+        public String Nom_cli { get; set; }
+        [DataMember]
+        public String Ape_cli { get; set; }
+      
+    }
+}
---
+++ WCF_Mant/ServicioVehiculos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Mant
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioVehiculos" en el código y en el archivo de configuración a la vez.
+    public class ServicioVehiculos : IServicioVehiculos
+    {
+        
+        public List<VehiculoDC> ListarVehiculosFechas(String strVehiculo, DateTime fecini, DateTime fecfin)
+        {
+            MANTENIMIENTO mant = new MANTENIMIENTO();
+            try
+            {
+                List<VehiculoDC> list = new List<VehiculoDC>();
+
+                var query = mant.usp_VehiculoFechas(strVehiculo, fecini, fecfin);
+
+                foreach (var resultado in query)
+                {
+                    VehiculoDC objVeh = new VehiculoDC();
+
+                    objVeh.idVehiculo = resultado.idVehiculo;                    
+                    objVeh.Fec_Mant_Inic = Convert.ToDateTime(resultado.Fec_Mant_Inic);
+                    objVeh.Fec_Mant_Fin = Convert.ToDateTime(resultado.Fec_Mant_Fin);
+                    objVeh.tipo_mantenimiento = Convert.ToSingle(resultado.tipo_mantenimiento);
+                    objVeh.Nom_mec = resultado.Nom_mec;
+                    objVeh.Ape_mec = resultado.Ape_mec;
+                    objVeh.idMecanico = resultado.idMecanico;
+                    objVeh.Tipo_Trans = Convert.ToSingle(resultado.Tipo_Trans);
+                    objVeh.idMarca = resultado.idMarca;
+                    objVeh.TipoUnidad=resultado.TipoUnidad;
+                    objVeh.Tipo_Unidad =Convert.ToSingle(objVeh.Tipo_Unidad);
+                    objVeh.Nom_cli = resultado.Nom_cli;
+                    objVeh.Ape_cli = resultado.Ape_cli;
+
+                    list.Add(objVeh);
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            
+        }
+    }
+}
